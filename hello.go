8b3497402cede6e7c20b2ba42053ab6edeaeb2e4
wilber <wilber_nilber@hotmail.com>
2020-05-07 15:19:31 -0300

    comando Scan

--- hello.go
+++ hello.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "reflect"
+)
 
 func main() {
-	fmt.Println("Olá Mundo")
+	// var nome string = "Wilber"
+	nome := "Wilber"
+	var versao float32 = 1.1
+	var comando int
+	fmt.Println("Olá Sr.", nome)
+	fmt.Println("A versão do programa é", versao)
+	// fmt.Println(reflect.TypeOf(nome))
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+	fmt.Scan(&comando)
+	fmt.Println("O comando escolhido foi", comando)
 }
