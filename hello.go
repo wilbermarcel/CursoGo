6559d97ab4fc7fb45ded6d98083c7c4b2dc05044
wilber <wilber_nilber@hotmail.com>
2020-05-07 17:08:34 -0300

    usando http - get

--- hello.go
+++ hello.go
@@ -2,20 +2,63 @@ package main
 
 import (
 	"fmt"
-	// "reflect"
+	"net/http"
+	"os"
 )
 
 func main() {
-	// var nome string = "Wilber"
-	nome := "Wilber"
-	var versao float32 = 1.1
-	var comando int
+	exibeIntroducao()
+	for {
+		exibeMenu()
+		trataMenu(leComando())
+	}
+}
+
+func exibeIntroducao() {
+	var (
+		nome   = "Wilber"
+		versao = 1.1
+	)
 	fmt.Println("Olá Sr.", nome)
 	fmt.Println("A versão do programa é", versao)
-	// fmt.Println(reflect.TypeOf(nome))
+}
+
+func exibeMenu() {
 	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
+}
+
+func leComando() int {
+	var comando int
 	fmt.Scan(&comando)
 	fmt.Println("O comando escolhido foi", comando)
+	return comando
+}
+
+func trataMenu(comando int) {
+	switch comando {
+	case 1:
+		iniciarMonitoramento()
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa...")
+		os.Exit(0)
+	default:
+		fmt.Println("Comando inválido")
+		os.Exit(-1)
+	}
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	// site := "https://www.alura.com.br"
+	site := "https://random-status-code.herokuapp.com/"
+	resp, _ := http.Get(site)
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
 }
